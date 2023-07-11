68dc355ab9e50bb5f7f4d59a985b52ea7cbf52d1
Khairul Basar <khairulbashar24853@gmail.com>
2023-07-11 10:24:28 +0600

    Updatedgit status

--- Number_2_(b)_prime_number.asm
+++ Number_2_(b)_prime_number.asm
@@ -1,63 +1,121 @@
-.MODEL SMALL
-.STACK 100H
-.DATA
-    PRIME_NUMBERS DB 2D,3D,5D,7D,'$'
-    INPUT_MSG DB 'ENTER YOUR NUMBER: $'
-    PRIME_MSG DB 0DH,0AH,'PRIME NUMBER$'
-    NOT_PRIME_MSG DB 0DH,0AH,'NOT PRIME NUMBER$'
-    NUM DB 0D
-.CODE
-MAIN PROC
-    MOV AX,@DATA
-    MOV DS,AX
-    MOV ES,AX
+.model small
+.stack 100h
+.data
 
-    ;SHOW INPUT PROMPT
-    MOV AH,9D
-    LEA DX,INPUT_MSG
-    INT 21H
+msg1 db 'Enter single digit : $'                                          .model small
+.stack 100h
+.data
 
-    ; TAKE INPUT
-    MOV AH,1
-    INT 21H
-    MOV NUM,AL
-    SUB NUM,48
+msg1 db 'Enter single digit : $'
 
-    MOV CX,4
-    MOV BL,2
+msg2 db 0dh,0ah,'Prime $'  
+msg3 db 0dh,0ah,'Not prime $'
 
-    CHECK_PRIME:
-        CMP NUM,0D ; Exceptional case for zero
-        JE NON_PRIME
 
-        CMP NUM,1D ;; Exceptional case for one
-        JE NON_PRIME
-        ;CHECK IF SI IS LESS THAN THE NUMBER
-        CMP BL,NUM
-        JGE PRIME
+.code
+main proc 
+    
+         mov ax,data
+         mov ds,ax
+         
+         mov ah,9
+         lea dx,msg1
+         int 21h
+         
+         mov ah,1
+         int 21h
+         sub al,48
+         
+         mov ah,0
+         mov bx,ax
+         
+         mov cx,ax
+         cmp cx,1
+         jle not_prime
+         
+         dec cx
+         top:
+            
+            cmp cx,1
+            jle prime
+            div cl
+            cmp ah,0
+            je not_prime
+            mov ax,bx
+            dec cx
+            jmp top         
+         
+         prime:
+            mov ah,9
+            lea dx,msg2
+            int 21h
+            jmp exit 
+            
+        not_prime:
+             mov ah,9
+            lea dx,msg3
+            int 21h
+    
+    
+   exit:
+    mov ah,4ch
+    int 21h
+    
+    
+main endp
 
-        XOR AX,AX ; need to clear AX because prev div operation change the value of AH, but we assume here AH is zero
-        MOV AL,NUM
-        DIV BL
-        CMP AH,0D
-        JE NON_PRIME
+msg2 db 0dh,0ah,'Prime $'  
+msg3 db 0dh,0ah,'Not prime $'
 
-        INC BL
-    JMP CHECK_PRIME
 
-    NON_PRIME:
-        LEA DX,NOT_PRIME_MSG
-        MOV AH,9D
-        INT 21H
-        JMP EXIT
-    PRIME:
-        LEA DX,PRIME_MSG
-        MOV AH,9D
-        INT 21H
-
-    EXIT:
-
-    MOV AH,4CH
-    INT 21H
-MAIN ENDP
-END MAIN
+.code
+main proc 
+    
+         mov ax,data
+         mov ds,ax
+         
+         mov ah,9
+         lea dx,msg1
+         int 21h
+         
+         mov ah,1
+         int 21h
+         sub al,48
+         
+         mov ah,0
+         mov bx,ax
+         
+         mov cx,ax
+         cmp cx,1
+         jle not_prime
+         
+         dec cx
+         top:
+            
+            cmp cx,1
+            jle prime
+            div cl
+            cmp ah,0
+            je not_prime
+            mov ax,bx
+            dec cx
+            jmp top         
+         
+         prime:
+            mov ah,9
+            lea dx,msg2
+            int 21h
+            jmp exit 
+            
+        not_prime:
+             mov ah,9
+            lea dx,msg3
+            int 21h
+    
+    
+   exit:
+    mov ah,4ch
+    int 21h
+    
+    
+main endp
